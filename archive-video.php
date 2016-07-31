e5c4d0b03fcd824017446c151db45998073bef35
David Gaultiere <dgaultiere@gmail.com>
2016-07-31 15:18:31 -0700

    modified wp_query on video archive to order by custom field video_date

--- archive-video.php
+++ archive-video.php
@@ -12,19 +12,33 @@
 <?php endif; ?>
 
 <div class="row">
-  <?php while (have_posts()) : the_post(); ?>
-    <div class="col-sm-4 grid-item">
-      <?php get_template_part('templates/content-archive', get_post_type() != 'post' ? get_post_type() : get_post_format()); ?>
-    </div>
   <?php
-    $count++;
-    if ( $count % 3 == 0 ) echo '</div><div class="row">';
-    endwhile;
+    $args = array(
+      'post_type' => 'video',
+      'meta_key' => 'video_date',
+      'orderby' => 'meta_value_num',
+      'order' => 'DESC'
+    );
+    // the query
+    $the_query = new WP_Query( $args );
+    if ( $the_query->have_posts() ) :
+      // the loop
+      while ( $the_query->have_posts() ) : $the_query->the_post();
+  ?>
+        <div class="col-sm-4 grid-item">
+          <?php get_template_part('templates/content-archive', get_post_type() != 'post' ? get_post_type() : get_post_format()); ?>
+        </div>
+  <?php
+        $count++;
+        if ( $count % 3 == 0 ) echo '</div><div class="row">';
+      endwhile; // end of the loop
+      wp_reset_postdata();
+    endif;
   ?>
 </div>
 
 <?php
-  if ($wp_query->max_num_pages > 1) :
+  if ($the_query->max_num_pages > 1) :
    Roots\Sage\Extras\page_navi();
   endif;
 ?>
