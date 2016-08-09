94f441ef93b0e82f1e9d1d31f5973f5afc34b4b6
David Gaultiere <dgaultiere@gmail.com>
2016-08-08 22:29:06 -0700

    make event time field required

--- plugins/acf/theme-field-groups.php
+++ plugins/acf/theme-field-groups.php
@@ -1968,7 +1968,7 @@ acf_add_local_field_group(array (
 			'name' => 'event_time',
 			'type' => 'text',
 			'instructions' => '',
-			'required' => 0,
+			'required' => 1,
 			'conditional_logic' => 0,
 			'wrapper' => array (
 				'width' => '',
