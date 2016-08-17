5ac1b661ef4822ad4bd6cf155c8d04554b934fa6
David Gaultiere <dgaultiere@gmail.com>
2016-08-16 21:44:20 -0700

    added acf-json to gitignore and added old template files for now (removing them later)

---
+++ templates/content-single-video-old.php
@@ -0,0 +1,16 @@
+<!-- Exactly like content-single.php but without a featured image -->
+<?php while (have_posts()) : the_post(); ?>
+  <article <?php post_class(); ?>>
+    <header>
+      <?php get_template_part('templates/entry-meta-video'); ?>
+    </header>
+    <div class="entry-content <?php if(the_field('text_style') !== 'sans-serif'){ ?>serif<?php }; ?>">
+      <?php the_content(); ?>
+    </div>
+	  <div class="clearfix"></div>
+    <footer>
+      <?php get_template_part('templates/share-buttons'); ?>
+    </footer>
+    <?php comments_template('/templates/comments.php'); ?>
+  </article>
+<?php endwhile; ?>
