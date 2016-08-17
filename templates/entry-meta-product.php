fdc414995ab1d349b59a79e2f94a3fa748002d7f
David Gaultiere <dgaultiere@gmail.com>
2016-08-16 22:42:04 -0700

    increased featured image size on product pages and changed design of author meta to use subtitle pattern

--- templates/entry-meta-product.php
+++ templates/entry-meta-product.php
@@ -3,7 +3,8 @@
 		<i class="fa fa-shopping-cart"></i><?php echo get_post_type(); ?>
 	</span>
 	<span class="meta-datapoint author">
-		<i class="fa fa-user"></i><?php the_field('product_author'); ?>
+		<!-- <i class="fa fa-user"></i><?php the_field('product_author'); ?> -->
+		<h4 class="entry-subtitle"><?php the_field('product_author'); ?></h4>
 	</span>
 	<span class="meta-datapoint price">
 		<i class="fa fa-dollar"></i><?php the_field('product_price'); ?>
