9cce948832690383ba3b22cb1dfb38c45b19bcfc
David Gaultiere <dgaultiere@gmail.com>
2016-08-16 22:43:04 -0700

    version 1.5.1

--- templates/entry-meta-product.php
+++ templates/entry-meta-product.php
@@ -3,7 +3,6 @@
 		<i class="fa fa-shopping-cart"></i><?php echo get_post_type(); ?>
 	</span>
 	<span class="meta-datapoint author">
-		<!-- <i class="fa fa-user"></i><?php the_field('product_author'); ?> -->
 		<h4 class="entry-subtitle"><?php the_field('product_author'); ?></h4>
 	</span>
 	<span class="meta-datapoint price">
