3c2893f4b78023245fca086ad9f2c5dd88b56354
David Gaultiere <dgaultiere@gmail.com>
2016-08-16 21:58:00 -0700

    updated search results to use blog sidebar rather than page sidebar

--- templates/sidebar.php
+++ templates/sidebar.php
@@ -3,7 +3,7 @@
 	{
 			dynamic_sidebar('sidebar-podcast');
 	}
-	elseif( is_single() || is_post_type_archive() || is_home() || is_author() || is_category() || is_date() || is_archive() )
+	elseif( is_single() || is_post_type_archive() || is_home() || is_author() || is_category() || is_date() || is_archive() || is_search() )
 	{
 	    dynamic_sidebar('sidebar-blog');
 	}
