fd167cb22335c9d917efa15adf57a5e07ebbdacb
David Gaultiere <dgaultiere@gmail.com>
2016-08-25 20:08:12 -0700

    changed content-snippet-post-full to use large thumbnail

--- templates/content-snippet-post-full.php
+++ templates/content-snippet-post-full.php
@@ -1,6 +1,6 @@
 <?php
   $thumb_id = get_post_thumbnail_id();
-  $thumb_url_array = wp_get_attachment_image_src($thumb_id, 'medium', false);
+  $thumb_url_array = wp_get_attachment_image_src($thumb_id, 'large');
   $thumb_url = $thumb_url_array[0];
   $excerpt = get_the_excerpt();
 ?>
