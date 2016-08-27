e6e356e1c67e7436e286688be87e0ec82d418d1f
David Gaultiere <dgaultiere@gmail.com>
2016-08-27 16:51:23 -0700

    added space between title and url for twitter share

--- templates/share-buttons.php
+++ templates/share-buttons.php
@@ -5,7 +5,7 @@
     $share_title = utf8_uri_encode( get_the_title(), 99 );
     $share_url = urlencode( get_permalink($post->ID) );
     $share_image = urlencode( get_site_url() . wp_get_attachment_url( get_post_thumbnail_id($post->ID) ) );
-    $share_description = utf8_uri_encode('I just found this ' . get_post_type($post->ID) . ' on ' . get_bloginfo('name') . ' and thought you would enjoy it.', 999) . '%0A%0A';
+    $share_description = utf8_uri_encode('I just found this ' . get_post_type($post->ID) . ' on ' . get_bloginfo('name') . ' and thought you would enjoy it.', 999);
 ?>
   <div class="social-share-container">
     <h4><?php the_field('post_share_title', 'option'); ?></h4>
@@ -19,7 +19,7 @@
       <?php }; ?>
       <?php if ( in_array( 'twitter', get_field('post_share_buttons', 'option') ) ) { ?>
         <li>
-          <a href="https://twitter.com/intent/tweet?text=<?php echo $share_title . $share_url ?>" target="_blank" class="twitter-icon">
+          <a href="https://twitter.com/intent/tweet?text=<?php echo $share_title . '%20' . $share_url ?>" target="_blank" class="twitter-icon">
             <i class="fa fa-twitter"></i>
           </a>
         </li>
@@ -33,7 +33,7 @@
       <?php }; ?>
       <?php if ( in_array( 'email', get_field('post_share_buttons', 'option') ) ) { ?>
         <li>
-          <a href="mailto:?subject=<?php echo $share_title ?>&body=<?php echo $share_description . $share_url ?>" target="_blank" class="email-icon">
+          <a href="mailto:?subject=<?php echo $share_title ?>&body=<?php echo $share_description . '%0A%0A' . $share_url ?>" target="_blank" class="email-icon">
             <i class="fa fa-envelope"></i>
           </a>
         </li>
