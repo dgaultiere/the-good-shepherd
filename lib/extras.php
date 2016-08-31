9b11cbea24460b0091f578f55901f1e536280ce6
David Gaultiere <dgaultiere@gmail.com>
2016-08-30 23:53:40 -0700

    added ... after function to limit string length

--- lib/extras.php
+++ lib/extras.php
@@ -353,7 +353,7 @@ function string_limit_words($string, $word_limit) {
   $words = explode(' ', $string, ($word_limit + 1));
   if(count($words) > $word_limit)
   array_pop($words);
-  return implode(' ', $words);
+  return implode(' ', $words) . '...';
 }
 add_filter('string_limit_words', __NAMESPACE__ . '\\string_limit_words');
 
