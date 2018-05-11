3773a98efd2429e053ac26798f5b9ffe22a23afa
David Gaultiere <dgaultiere@gmail.com>
2018-05-11 13:44:32 -0400

    added sections and titles/subtitles to home-2 template and acf options

--- template-home-2.php
+++ template-home-2.php
@@ -18,14 +18,14 @@
 				<?php if ( get_field('show_cover_button') ) { ?>
 					<a href="<?php the_field('cover_button_link') ?>" class="btn btn-primary"><?php the_field('cover_button_text') ?></a>
 				<?php }; ?>
-				<a href="#recent-articles" class="down-arrow"><i class="fa fa-angle-down"></i></a>
+				<a href="#featured-link" class="down-arrow"><i class="fa fa-angle-down"></i></a>
 			</div>
 		</div>
 	</div>
 </div>
 
 <!-- Featured Link -->
-<div class="container-fluid section grey-bg" id="featured-link">
+<div class="container-fluid section" id="featured-link">
 	<div class="row">
 		<div class="container">
 			<div class="row">
@@ -47,14 +47,34 @@
 		</div>
 	</div>
 	<div class="row">
-		<a href="#featured-product" class="down-arrow"><i class="fa fa-angle-down"></i></a>
+		<a href="#podcast" class="down-arrow"><i class="fa fa-angle-down"></i></a>
 	</div>
 </div>
 
 <!-- Podcast -->
+<div class="container-fluid section grey-bg" id="podcast">
+	<div class="row">
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-12">
+					<h2 class="title"><?php the_field('podcast_title'); ?></h2>
+					<h3 class="subtitle"><?php the_field('podcast_subtitle'); ?></h3>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-xs-12">
+					<!-- stuff here -->
+				</div>
+			</div>
+			<div class="row">
+				<a href="#product" class="down-arrow"><i class="fa fa-angle-down"></i></a>
+			</div>
+		</div>
+	</div>
+</div>
 
 <!-- Featured Product -->
-<div class="container-fluid section" id="featured-product">
+<div class="container-fluid section" id="product">
 	<div class="row">
 		<div class="container">
 			<div class="row">
@@ -75,6 +95,9 @@
 					</div>
 					<a href="<?php the_field('featured_product_link') ?>" class="btn btn-primary">More Info <i class="fa fa-angle-right"></i></a>
 				</div>
+			</div>
+			<div class="row">
+				<a href="#about" class="down-arrow"><i class="fa fa-angle-down"></i></a>
 			</div>
 		</div>
 	</div>
@@ -113,15 +136,72 @@
 				</div>
 			</div>
 			<div class="row">
-				<a href="#featured" class="down-arrow"><i class="fa fa-angle-down"></i></a>
+				<a href="#video" class="down-arrow"><i class="fa fa-angle-down"></i></a>
 			</div>
 		</div>
 	</div>
 </div>
 
 <!-- Video -->
+<div class="container-fluid section" id="video">
+	<div class="row">
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-12">
+					<h2 class="title"><?php the_field('video_title'); ?></h2>
+					<h3 class="subtitle"><?php the_field('video_subtitle'); ?></h3>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-xs-12">
+					<!-- stuff here -->
+				</div>
+			</div>
+			<div class="row">
+				<a href="#highlight" class="down-arrow"><i class="fa fa-angle-down"></i></a>
+			</div>
+		</div>
+	</div>
+</div>
 
-<!-- Description #2 -->
+<!-- Highlighted Page -->
+<div class="container-fluid section grey-bg" id="highlight">
+	<div class="row">
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-12">
+					<h2 class="title"><?php the_field('highlight_title'); ?></h2>
+					<h3 class="subtitle"><?php the_field('highlight_subtitle'); ?></h3>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-xs-12">
+					<?php echo wp_get_attachment_image( get_field('highlight_image'), 'medium' ); ?>
+					<a href="<?php the_field('highlight_link'); ?>" class="heading-link">
+						<h4><?php the_field('highlight_paragraph_heading'); ?></h4>
+					</a>
+					<div>
+						<p>
+							<?php the_field('highlight_paragraph_content'); ?>
+						</p>
+						<div class="read-more-link">
+							<a href="<?php the_field('highlight_link'); ?>">
+					  		<?php if(get_field('site_language', 'option') == "es-mx") { ?>
+					  			Lee Mas
+								<?php } else { ?>
+									Read More
+								<?php } ?>
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<a href="#subscribe" class="down-arrow"><i class="fa fa-angle-down"></i></a>
+			</div>
+		</div>
+	</div>
+</div>
 
 <!-- Subscribe -->
 <div class="container-fluid section text-<?php the_field('subscribe_text_color'); ?>" id="subscribe" style="background-color:<?php the_field('subscribe_background_color'); ?>">
@@ -140,3 +220,20 @@
 </div>
 
 <!-- Search -->
+<div class="container-fluid section grey-bg" id="search">
+	<div class="row">
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-12">
+					<h2 class="title"><?php the_field('search_title'); ?></h2>
+					<h3 class="subtitle"><?php the_field('search_subtitle'); ?></h3>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-xs-12">
+					<!-- stuff here -->
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
