4aeb37b7f054ed5aa59133c8beb90abc8ad7f5ac
David Gaultiere <dgaultiere@gmail.com>
2018-05-26 17:18:48 -0700

    removing dates from posts

--- templates/entry-meta-podcast.php
+++ templates/entry-meta-podcast.php
@@ -7,9 +7,6 @@
 			<i class="fa fa-user"></i><?php the_field('podcast_author'); ?>
 		</div>
 	<?php }; ?>
-	<div class="meta-datapoint date">
-		<i class="fa fa-calendar"></i><?php the_time( get_option( 'date_format' ) ); ?>
-	</div>
 	<?php if( get_the_terms($post->ID, 'podcast_tags') ) { ?>
 		<div class="meta-datapoint tags">
 			<i class="fa fa-tags"></i>Tags: <?php the_terms( $post->ID, 'podcast_tags' ,  ' ' ); ?>
--- templates/entry-meta-video.php
+++ templates/entry-meta-video.php
@@ -10,9 +10,6 @@
 			<i class="fa fa-map-marker"></i><?php the_field('video_location'); ?>
 		</div>
 	<?php }; ?>
-	<div class="meta-datapoint date">
-		<i class="fa fa-calendar"></i><?php the_field('video_date'); ?>
-	</div>
 	<?php if ( get_field('show_video_length') ) { ?>
 		<div class="meta-datapoint length">
 			<i class="fa fa-clock-o"></i><?php the_field('video_length'); ?>
