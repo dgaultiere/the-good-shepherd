f2430991e2cb89a7b8f762566c165b5a2e60c70f
David Gaultiere <dgaultiere@gmail.com>
2018-06-03 09:04:58 -0700

    all the homepage content fields

--- template-home-2.php
+++ template-home-2.php
@@ -13,9 +13,18 @@
 					<div class="col-xs-12">
 						<h1 class="title"><?php the_field('cover_title'); ?></h1>
 						<h2 class="subtitle"><?php the_field('cover_subtitle'); ?></h2>
-						<?php if ( get_field('show_cover_button') ) { ?>
-							<a href="<?php the_field('cover_button_link'); ?>" class="btn btn-primary"><?php the_field('cover_button_text'); ?></a>
-						<?php }; ?>
+						<?php
+							if ( get_field('show_cover_button') ) :
+								$button = get_field('cover_button');
+								if( $button ) :
+						?>
+							<a href="<?php echo $button['url']; ?>" target="<?php echo $button['target']; ?>" class="btn btn-primary">
+								<?php echo $button['title']; ?>
+							</a>
+						<?php
+								endif;
+							endif;
+						?>
 					</div>
 				</div>
 				<div class="row">
@@ -50,13 +59,18 @@
 					</a>
 				</div>
 			</div>
-			<div class="row">
-				<div class="col-xs-12">
-					<a href="<?php the_field('featured_link_button_url'); ?>" class="btn btn-primary">
-						<?php the_field('featured_link_button_text'); ?>
-					</a>
+			<?php
+				$button = get_field('featured_link_button');
+				if( $button ):
+			?>
+				<div class="row">
+					<div class="col-xs-12">
+						<a href="<?php echo $button['url']; ?>" target="<?php echo $button['target']; ?>" class="btn btn-primary">
+							<?php echo $button['title']; ?>
+						</a>
+					</div>
 				</div>
-			</div>
+			<?php endif; ?>
 			<div class="row">
 				<a href="#testimonial" class="down-arrow"><i class="fa fa-angle-down"></i></a>
 			</div>
@@ -82,13 +96,18 @@
 					</div>
 				</div>
 			</div>
-			<div class="row">
-				<div class="col-xs-12">
-					<a href="<?php the_field('testimonial_link_url'); ?>" class="secondary-link">
-						<?php the_field('testimonial_link_text'); ?><i class="fa fa-angle-right"></i>
-					</a>
+			<?php
+				$link = get_field('testimonial_action_link');
+				if( $link ):
+			?>
+				<div class="row">
+					<div class="col-xs-12">
+						<a href="<?php echo $link['url']; ?>" target="<?php echo $link['target']; ?>" class="secondary-link">
+							<?php echo $link['title']; ?><i class="fa fa-angle-right"></i>
+						</a>
+					</div>
 				</div>
-			</div>
+			<?php endif; ?>
 			<div class="row">
 				<a href="#what-we-offer" class="down-arrow"><i class="fa fa-angle-down"></i></a>
 			</div>
@@ -105,49 +124,49 @@
 					<h2 class="title"><?php the_field('wwo_title'); ?></h2>
 				</div>
 			</div>
-			<div class="row">
-				<?php if( have_rows('wwo_distinctives') ): while ( have_rows('wwo_distinctives') ) : the_row(); ?>
-					<div class="col-sm-4 distinctive">
-						<?php
-							$icon = get_sub_field('icon');
-							if( $icon ) {
-								echo wp_get_attachment_image( $icon, 'thumbnail');
-							}
-						?>
-						<h4><?php the_sub_field('label'); ?></h4>
-						<p>
+			<?php if( have_rows('wwo_distinctives') ): ?>
+				<div class="row">
+					<?php while ( have_rows('wwo_distinctives') ) : the_row(); ?>
+						<div class="col-sm-4 distinctive">
 							<?php
-								if( have_rows('links') ):
-								while ( have_rows('links') ) : the_row();
-								$link = get_sub_field('link');
-								if( $link ):
+								$icon = get_sub_field('icon');
+								if( $icon ) {
+									echo wp_get_attachment_image( $icon, 'thumbnail');
+								}
 							?>
-								<a href="<?php echo $link['url']; ?>" target="<?php echo $link['target']; ?>">
-									<?php echo $link['title']; ?>
-								</a>
-							<?php
-								endif;
-								endwhile;
-								endif;
-							?>
-						</p>
-					</div>
-				<?php endwhile; endif; ?>
-			</div>
-			<div class="row">
-				<div class="col-xs-12">
-					<?php
-						$button = get_field('wwo_button');
-						if( $button ):
-					?>
+							<h4><?php the_sub_field('label'); ?></h4>
+							<p>
+								<?php
+									if( have_rows('links') ):
+									while ( have_rows('links') ) : the_row();
+									$link = get_sub_field('link');
+									if( $link ):
+								?>
+									<a href="<?php echo $link['url']; ?>" target="<?php echo $link['target']; ?>">
+										<?php echo $link['title']; ?>
+									</a>
+								<?php
+									endif;
+									endwhile;
+									endif;
+								?>
+							</p>
+						</div>
+					<?php endwhile; ?>
+				</div>
+			<?php endif; ?>
+			<?php
+				$button = get_field('wwo_button');
+				if( $button ):
+			?>
+				<div class="row">
+					<div class="col-xs-12">
 						<a href="<?php echo $button['url']; ?>" target="<?php echo $button['target']; ?>" class="btn btn-primary">
 							<?php echo $button['title']; ?>
 						</a>
-					<?php
-						endif;
-					?>
+					</div>
 				</div>
-			</div>
+			<?php endif; ?>
 			<div class="row">
 				<a href="#about" class="down-arrow"><i class="fa fa-angle-down"></i></a>
 			</div>
@@ -171,20 +190,18 @@
 					<p class="text-justify"><?php the_field('about_description'); ?></p>
 				</div>
 			</div>
-			<div class="row">
-				<div class="col-xs-12">
-					<?php
-						$link = get_field('about_link');
-						if( $link ):
-					?>
+			<?php
+				$link = get_field('about_link');
+				if( $link ):
+			?>
+				<div class="row">
+					<div class="col-xs-12">
 						<a href="<?php echo $link['url']; ?>" target="<?php echo $link['target']; ?>" class="secondary-link">
 							<?php echo $link['title']; ?><i class="fa fa-angle-right"></i>
 						</a>
-					<?php
-						endif;
-					?>
+					</div>
 				</div>
-			</div>
+			<?php endif; ?>
 			<div class="row">
 				<a href="#video" class="down-arrow"><i class="fa fa-angle-down"></i></a>
 			</div>
@@ -205,20 +222,30 @@
 					</div>
 				</div>
 			</div>
-			<div class="row">
-				<div class="col-xs-12">
-					<a href="<?php the_field('video_button_url'); ?>" class="btn btn-primary">
-						<?php the_field('video_button_text'); ?>
-					</a>
+			<?php
+				$button = get_field('video_button');
+				if( $button ):
+			?>
+				<div class="row">
+					<div class="col-xs-12">
+						<a href="<?php echo $button['url']; ?>" target="<?php echo $button['target']; ?>" class="btn btn-primary">
+							<?php echo $button['title']; ?>
+						</a>
+					</div>
 				</div>
-			</div>
-			<div class="row">
-				<div class="col-xs-12">
-					<a href="<?php the_field('video_secondary_url'); ?>" class="secondary-link">
-						<?php the_field('video_secondary_text'); ?><i class="fa fa-angle-right"></i>
-					</a>
+			<?php endif; ?>
+			<?php
+				$link = get_field('video_link');
+				if( $link ):
+			?>
+				<div class="row">
+					<div class="col-xs-12">
+						<a href="<?php echo $link['url']; ?>" target="<?php echo $link['target']; ?>" class="secondary-link">
+							<?php echo $link['title']; ?><i class="fa fa-angle-right"></i>
+						</a>
+					</div>
 				</div>
-			</div>
+			<?php endif; ?>
 			<div class="row">
 				<a href="#highlight" class="down-arrow"><i class="fa fa-angle-down"></i></a>
 			</div>
@@ -242,49 +269,45 @@
 					<p class="text-justify"><?php the_field('highlight_description'); ?></p>
 				</div>
 			</div>
-			<div class="row">
-				<div class="col-sm-4 distinctive">
-					<?php
-						$distinctive_image_1 = get_field('highlight_distinctive_1_icon');
-						if( $distinctive_image_1 ) {
-							echo wp_get_attachment_image( $distinctive_image_1, 'thumbnail');
-						}
-					?>
-					<h4><?php the_field('highlight_distinctive_1'); ?></h4>
+			<?php if( have_rows('hp_distinctives') ): ?>
+				<div class="row">
+					<?php while ( have_rows('hp_distinctives') ) : the_row(); ?>
+						<div class="col-sm-4 distinctive">
+							<?php
+								$icon = get_sub_field('icon');
+								if( $icon ) {
+									echo wp_get_attachment_image( $icon, 'thumbnail');
+								}
+							?>
+							<h5><?php the_sub_field('label'); ?></h5>
+						</div>
+					<?php endwhile; ?>
 				</div>
-				<div class="col-sm-4 distinctive">
-					<?php
-						$distinctive_image_2 = get_field('highlight_distinctive_2_icon');
-						if( $distinctive_image_2 ) {
-							echo wp_get_attachment_image( $distinctive_image_2, 'thumbnail');
-						}
-					?>
-					<h4><?php the_field('highlight_distinctive_2'); ?></h4>
+			<?php endif; ?>
+			<?php
+				$button = get_field('highlight_button');
+				if( $button ):
+			?>
+				<div class="row">
+					<div class="col-xs-12">
+						<a href="<?php echo $button['url']; ?>" target="<?php echo $button['target']; ?>" class="btn btn-primary">
+							<?php echo $button['title']; ?>
+						</a>
+					</div>
 				</div>
-				<div class="col-sm-4 distinctive">
-					<?php
-						$distinctive_image_3 = get_field('highlight_distinctive_3_icon');
-						if( $distinctive_image_3 ) {
-							echo wp_get_attachment_image( $distinctive_image_3, 'thumbnail');
-						}
-					?>
-					<h4><?php the_field('highlight_distinctive_3'); ?></h4>
+			<?php endif; ?>
+			<?php
+				$link = get_field('highlight_link');
+				if( $link ):
+			?>
+				<div class="row">
+					<div class="col-xs-12">
+						<a href="<?php echo $link['url']; ?>" target="<?php echo $link['target']; ?>" class="secondary-link">
+							<?php echo $link['title']; ?><i class="fa fa-angle-right"></i>
+						</a>
+					</div>
 				</div>
-			</div>
-			<div class="row">
-				<div class="col-xs-12">
-					<a href="<?php the_field('highlight_button_url'); ?>" class="btn btn-primary">
-						<?php the_field('highlight_button_text'); ?>
-					</a>
-				</div>
-			</div>
-			<div class="row">
-				<div class="col-xs-12">
-					<a href="<?php the_field('highlight_secondary_url'); ?>" class="secondary-link">
-						<?php the_field('highlight_secondary_text'); ?><i class="fa fa-angle-right"></i>
-					</a>
-				</div>
-			</div>
+			<?php endif; ?>
 			<div class="row">
 				<a href="#featured-quotes" class="down-arrow"><i class="fa fa-angle-down"></i></a>
 			</div>
@@ -351,20 +374,43 @@
 						<p><?php the_field('podcast_description'); ?></p>
 					</div>
 				</div>
-				<div class="row">
-					<div class="col-xs-12">
-						<a href="<?php the_field('podcast_button_url'); ?>" class="btn btn-primary">
-							<?php the_field('podcast_button_text'); ?>
-						</a>
+				<?php
+					$args = array(
+						'post_type' => 'podcast',
+						'posts_per_page' => '1'
+					);
+					$the_query = new WP_Query( $args );
+					if ( $the_query->have_posts() ) :
+						while ( $the_query->have_posts() ) : $the_query->the_post();
+				?>
+					<div class="row">
+						<div class="col-xs-12">
+							<a href="<?php the_permalink(); ?>" class="btn btn-primary">
+								Listen Now
+							</a>
+						</div>
 					</div>
-				</div>
-				<div class="row">
-					<div class="col-xs-12">
-						<a href="<?php the_field('podcast_secondary_url'); ?>" class="secondary-link">
-							<?php the_field('podcast_secondary_text'); ?><i class="fa fa-angle-right"></i>
-						</a>
+				<?php
+						endwhile;
+						wp_reset_postdata();
+					else :
+				?>
+					<a href="<?php echo get_post_type_archive_link('podcast') ?>" class="btn btn-primary">
+						View Episodes
+					</a>
+				<?php endif; ?>
+				<?php
+					$link = get_field('podcast_link');
+					if( $link ):
+				?>
+					<div class="row">
+						<div class="col-xs-12">
+							<a href="<?php echo $link['url']; ?>" target="<?php echo $link['target']; ?>" class="secondary-link">
+								<?php echo $link['title']; ?><i class="fa fa-angle-right"></i>
+							</a>
+						</div>
 					</div>
-				</div>
+				<?php endif; ?>
 				<div class="row">
 					<a href="#blog" class="down-arrow"><i class="fa fa-angle-down"></i></a>
 				</div>
@@ -374,7 +420,7 @@
 </div>
 
 <!-- Blog -->
-<div class="container-fluid section" id="blog">
+<div class="container-fluid section grey-bg" id="blog">
 	<div class="row">
 		<div class="container">
 			<div class="row">
@@ -388,7 +434,7 @@
 						$args = array('posts_per_page' => '1');
 						$the_query = new WP_Query( $args );
 						if ( $the_query->have_posts() ) :
-						while ( $the_query->have_posts() ) : $the_query->the_post();
+							while ( $the_query->have_posts() ) : $the_query->the_post();
 					?>
 					<?php	get_template_part('templates/content-snippet-post-side'); ?>
 				</div>
@@ -399,8 +445,8 @@
 						Read Now
 					</a>
 					<?php
-						endwhile;
-						wp_reset_postdata();
+							endwhile;
+							wp_reset_postdata();
 						else :
 					?>
 						<a href="<?php echo get_post_type_archive_link('post') ?>" class="btn btn-primary">
@@ -431,7 +477,7 @@
 </div>
 
 <!-- Featured Product -->
-<div class="container-fluid section grey-bg" id="product">
+<div class="container-fluid section" id="product">
 	<div class="row">
 		<div class="container">
 			<div class="row">
@@ -450,13 +496,23 @@
 					</a>
 				</div>
 				<div class="col-sm-5 col-sm-pull-2 pull-right">
-					<a href="<?php the_field('featured_product_button_url'); ?>" class="btn btn-primary">
-						<?php the_field('featured_product_button_text'); ?>
-					</a>
-					<div class="clearfix"></div>
-					<a href="<?php the_field('featured_product_secondary_url'); ?>" class="secondary-link">
-						<?php the_field('featured_product_secondary_text'); ?><i class="fa fa-angle-right"></i>
-					</a>
+					<?php
+						$button = get_field('fp_button');
+						if( $button ):
+					?>
+						<a href="<?php echo $button['url']; ?>" target="<?php echo $button['target']; ?>" class="btn btn-primary">
+							<?php echo $button['title']; ?>
+						</a>
+					<?php endif; ?>
+					<?php
+						$link = get_field('fp_link');
+						if( $link ):
+					?>
+						<div class="clearfix"></div>
+						<a href="<?php echo $link['url']; ?>" target="<?php echo $link['target']; ?>" class="secondary-link">
+							<?php echo $link['title']; ?><i class="fa fa-angle-right"></i>
+						</a>
+					<?php endif; ?>
 				</div>
 			</div>
 			<div class="row">
@@ -467,7 +523,7 @@
 </div>
 
 <!-- Search -->
-<div class="container-fluid section" id="search">
+<div class="container-fluid section grey-bg" id="search">
 	<div class="row">
 		<div class="container">
 			<div class="row">
@@ -482,13 +538,18 @@
 					</div>
 				</div>
 			</div>
-			<div class="row">
-				<div class="col-xs-12">
-					<a href="<?php the_field('search_secondary_url'); ?>" class="secondary-link">
-						<?php the_field('search_secondary_text'); ?><i class="fa fa-angle-right"></i>
-					</a>
+			<?php
+				$link = get_field('search_link');
+				if( $link ):
+			?>
+				<div class="row">
+					<div class="col-xs-12">
+						<a href="<?php echo $link['url']; ?>" target="<?php echo $link['target']; ?>" class="secondary-link">
+							<?php echo $link['title']; ?><i class="fa fa-angle-right"></i>
+						</a>
+					</div>
 				</div>
-			</div>
+			<?php endif; ?>
 		</div>
 	</div>
 </div>
