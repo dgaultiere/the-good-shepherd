a956eb02210fd1e72af8e1f42cfd1b6fd3663767
David Gaultiere <dgaultiere@gmail.com>
2018-07-08 22:28:57 -0700

    removing duplicate typekit scripts

--- templates/head.php
+++ templates/head.php
@@ -24,8 +24,6 @@
         h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
       })(document);
     </script>
-    <!-- <script src="https://use.typekit.net/ifd6ekz.js"></script>
-    <script>try{Typekit.load({ async: true });}catch(e){}</script> -->
 		<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet">
 
 		<!-- Theme Styles -->
